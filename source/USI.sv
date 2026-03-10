64871d4ec543cca25dfcf2629d2832674f88b005
Jared Settler <jsettler@purdue.edu>
2026-03-10 13:42:26 -0400

    top level integration

--- source/USI.sv
+++ source/USI.sv
@@ -3,6 +3,7 @@ module USI(
     bus_protocol_if.peripheral_vital bpif
 );
 
+    // reg_map signals
     logic [1:0]  mode_sel;
     logic [31:0] clkdiv;
     logic [31:0] parameters;
@@ -11,6 +12,18 @@ module USI(
 
     logic        ctrl_unit_error;
     logic [31:0] buffer_read;
+
+    // data_buffer signals
+    logic [7:0] data_in;
+    logic [31:0] buffer_write;
+    logic push;
+    logic pop;
+    logic clear;
+    logic load;
+    logic send;
+    logic [7:0] data_out;
+    logic [31:0] buffer_read;
+    logic [7:0] buffer_occupancy;
 
     reg_map REG_MAP (
         .bpif(bpif),
@@ -23,6 +36,22 @@ module USI(
         .parameters(parameters),
         .tx_data(tx_data),
         .error_reg(error_reg)
+    );
+
+    data_buffer FIFO (
+        .CLK(CLK),
+        .nRST(nRST),
+        .mode_sel(mode_sel),
+        .data_in(data_in),
+        .buffer_write(buffer_write),
+        .push(push),
+        .pop(pop),
+        .clear(clear),
+        .load(load),
+        .send(send),
+        .data_out(data_out),
+        .buffer_read(buffer_read),
+        .buffer_occupancy(buffer_occupancy)
     );
 
 endmodule
