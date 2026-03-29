0b6b95b1b30688253c4b732dbaea6179527a6df7
Jason Klutho <jklutho@purdue.edu>
2026-03-29 18:34:13 -0400

    Datapath testing and added full-duplex in datapath

--- testbench/datapath_tb.sv
+++ testbench/datapath_tb.sv
@@ -1,5 +1,3 @@
-`timescale 1ns/1ps
-
 module datapath_tb;
 
   logic clk;
@@ -12,7 +10,6 @@ module datapath_tb;
   logic tx_enable;
   logic msb_first;
   logic [1:0] parity_mode;
-  logic [2:0] data_bits;
   logic [7:0] data_out;
   logic start_bit_det;
   logic parity_error;
@@ -36,7 +33,6 @@ module datapath_tb;
     .tx_enable(tx_enable),
     .msb_first(msb_first),
     .parity_mode(parity_mode),
-    .data_bits(data_bits),
     .data_out(data_out),
     .start_bit_det(start_bit_det),
     .parity_error(parity_error),
@@ -65,7 +61,6 @@ module datapath_tb;
       tx_enable = 0;
       msb_first = 0;
       parity_mode = 0;
-      data_bits = 0;
       data_out = 0;
       serial_clk_tick();
       n_rst = 1;
@@ -78,14 +73,13 @@ module datapath_tb;
     input logic start_bit, 
     input logic stop_bit,
     input logic [1:0] parity, 
-    input logic [2:0] num_data_bits, 
     input logic msb_first
   );
     begin
+      @(negedge clk);
       start_bit_en = start_bit;
       stop_bit_en = stop_bit;
       parity_mode = parity;
-      data_bits = num_data_bits;
       msb_first = msb_first;
       rx_enable = 1;
 
@@ -96,7 +90,7 @@ module datapath_tb;
       end
 
       // Send data bits
-      for (int i = 0; i < num_data_bits; i++) begin
+      for (int i = 0; i < 8; i++) begin
         if (msb_first) begin
           serial_in = data[7 - i];
         end else begin
@@ -132,20 +126,18 @@ module datapath_tb;
     input logic start_bit, 
     input logic stop_bit,
     input logic [1:0] parity, 
-    input logic [2:0] num_data_bits, 
     input logic msb_first, 
   );
     begin
+      @(negedge clk);
       start_bit_en = start_bit;
       stop_bit_en = stop_bit;
       tx_enable = 1;
       parity_mode = parity;
-      data_bits = num_data_bits;
       msb_first = msb_first;
       data_out = data;
 
       // Wait for transmission to complete
-      // repeat ({29'b0, num_data_bits}) @(negedge serial_clk);
       repeat (12) serial_clk_tick();
       tx_enable = 0; // Done transmitting byte
     end
@@ -157,13 +149,14 @@ module datapath_tb;
     reset();
 
     // Test case 1: Receive byte with start bit, stop bit, even parity, LSB first
-    rx_byte(8'hA5, 1, 1, 2'b01, 3'b111, 0);
+    rx_byte(8'hA5, 1, 1, 2'b01, 0);
     #10;
 
     // Test case 2: Transmit byte with start bit, stop bit, odd parity, LSB first
-    tx_byte(8'h3C, 1, 1, 2'b10, 3'b111, 0);
+    tx_byte(8'h3C, 1, 1, 2'b10, 0);
     #10;
 
+    $display("Testbench ran");
     $finish;
   end
 
