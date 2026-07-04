e4f94f41bb21927369f2419b19aebb745a15c80a
Jason Klutho <jklutho@purdue.edu>
2026-07-04 15:09:49 -0400

    Simplify shift_register

--- source/shift_register.sv
+++ source/shift_register.sv
@@ -1,62 +1,41 @@
 module shift_register (
     input logic CLK,
     input logic nRST,
-    input logic start,
+    input logic parallel_load,
     input logic [7:0] parallel_in,
     input logic shift_en,
     input logic msb_first,
     input logic serial_in,
     output logic serial_out,
-    output logic [7:0] parallel_out,
-    output logic done
+    output logic [7:0] parallel_out
 );
-    logic [7:0] parallel_latch, next_parallel_latch, next_parallel_out;
-    logic [2:0] bit_count, next_bit_count;
+    logic [7:0] parallel_reg, next_parallel_reg;
+
+    assign parallel_out = parallel_reg;
+    assign serial_out = msb_first ? parallel_reg[7] : parallel_reg[0];
 
     always_ff @(posedge CLK, negedge nRST) begin
         if (~nRST) begin
-            parallel_out <= 8'b0;
-            bit_count <= 3'b0;
+            parallel_reg <= '0;
         end
         else begin
-            parallel_out <= next_parallel_out;
-            bit_count <= next_bit_count;
+            parallel_reg <= next_parallel_reg;
         end
     end
 
     always_comb begin
-        next_bit_count = bit_count;
-        next_parallel_latch = parallel_latch;
-        if (start) begin
-            next_bit_count = 3'b0;
-            next_parallel_latch = parallel_in;
+        next_parallel_reg = parallel_reg;
+        if (parallel_load) begin
+            next_parallel_reg = parallel_in;
         end
         else if (shift_en) begin
-            next_bit_count = bit_count + 1;
-        end
-    end
-
-    always_comb begin
-        next_parallel_out = parallel_out;
-        if (shift_en) begin
             if (msb_first) begin
-                next_parallel_out = {parallel_out[6:0], serial_in};
+                next_parallel_reg = {parallel_reg[6:0], serial_in};
             end
             else begin
-                next_parallel_out = {serial_in, parallel_out[7:1]};
+                next_parallel_reg = {serial_in, parallel_reg[7:1]};
             end
         end
     end
-
-    always_comb begin
-        if (msb_first) begin
-            serial_out = parallel_in[7 - bit_count];
-        end
-        else begin
-            serial_out  = parallel_in[bit_count];
-        end
-    end
-
-    assign done = (bit_count == 3'b111) ? 1'b1 : 1'b0;
 
 endmodule
