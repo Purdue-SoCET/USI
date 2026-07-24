9c6922ae0e17662ec3f5ed0dff0ba5ca6594cc79
Jason Klutho <jklutho@purdue.edu>
2026-07-23 23:36:07 -0400

    Parameterize the number of clock divider bits

--- source/register_map.sv
+++ source/register_map.sv
@@ -1,6 +1,7 @@
 module register_map #(
     parameter unsigned RX_FIFO_SIZE = 16,
-    parameter unsigned TX_FIFO_SIZE = 16
+    parameter unsigned TX_FIFO_SIZE = 16,
+    parameter int CLKDIV_BITS = 16
 )(
     bus_protocol_if.peripheral_vital bpif,
     input logic CLK,
@@ -17,14 +18,14 @@ module register_map #(
     input logic [$clog2(RX_FIFO_SIZE+1)-1:0] rx_count,
     input logic [31:0] rx_rdata,
     output logic [1:0] mode_sel,
-    output logic [15:0] clkdiv,
+    output logic [CLKDIV_BITS-1:0] clkdiv,
     output logic [31:0] tx_wdata,
     output logic [2:0] uart_config,
     output logic [7:0] spi_config,
     output logic i2c_config,
     output logic [9:0] i2c_addr,
-    output logic [31:0] i2c_t_low,
-    output logic [31:0] i2c_t_high,
+    output logic [CLKDIV_BITS-1:0] i2c_t_low,
+    output logic [CLKDIV_BITS-1:0] i2c_t_high,
     output logic tx_WEN,
     output logic rx_REN,
     output logic tx_clear_overrun,
@@ -35,13 +36,13 @@ module register_map #(
     output logic rx_flush
 );
     logic [1:0] next_mode_sel;
-    logic [15:0] next_clkdiv;
+    logic [CLKDIV_BITS:0] next_clkdiv;
     logic [2:0] next_uart_config;
     logic [7:0] next_spi_config;
     logic next_i2c_config;
     logic [9:0] next_i2c_addr;
-    logic [31:0] next_i2c_t_low;
-    logic [31:0] next_i2c_t_high;
+    logic [CLKDIV_BITS:0] next_i2c_t_low;
+    logic [CLKDIV_BITS:0] next_i2c_t_high;
     logic strobe_error, write_error, read_error;
 
     always_ff @(posedge CLK, negedge nRST) begin
@@ -84,14 +85,14 @@ module register_map #(
         if (bpif.wen && !strobe_error) begin
             case (bpif.addr)
                 32'h00: next_mode_sel = bpif.wdata[1:0];
-                32'h04: next_clkdiv = bpif.wdata[15:0];
+                32'h04: next_clkdiv = bpif.wdata[CLKDIV_BITS-1:0];
                 32'h08: {rx_clear_underrun, rx_clear_overrun, tx_clear_underrun, tx_clear_overrun} = bpif.wdata[3:0];
                 32'h0C: next_uart_config = bpif.wdata[2:0];
                 32'h10: next_spi_config = bpif.wdata[7:0];
                 32'h14: next_i2c_config = bpif.wdata[0];
                 32'h18: next_i2c_addr = bpif.wdata[9:0];
-                32'h1C: next_i2c_t_low = bpif.wdata;
-                32'h20: next_i2c_t_high = bpif.wdata;
+                32'h1C: next_i2c_t_low = bpif.wdata[CLKDIV_BITS-1:0];
+                32'h20: next_i2c_t_high = bpif.wdata[CLKDIV_BITS-1:0];
                 32'h24: tx_WEN = 1'b1;
                 32'h28: {rx_flush, tx_flush} = bpif.wdata[1:0];
                 // 32'h2C: read only
@@ -109,14 +110,14 @@ module register_map #(
         if (bpif.ren && !strobe_error) begin
             case (bpif.addr)
                 32'h00: bpif.rdata = {30'b0, mode_sel};
-                32'h04: bpif.rdata = {16'b0, clkdiv};
+                32'h04: bpif.rdata[CLKDIV_BITS-1:0] = clkdiv;
                 32'h08: bpif.rdata = {24'b0, rx_empty, rx_full, tx_empty, tx_full, rx_underrun, rx_overrun, tx_underrun, tx_overrun};
                 32'h0C: bpif.rdata = {29'b0, uart_config};
                 32'h10: bpif.rdata = {24'b0, spi_config};
                 32'h14: bpif.rdata = {31'b0, i2c_config};
                 32'h18: bpif.rdata = {22'b0, i2c_addr};
-                32'h1C: bpif.rdata = i2c_t_low;
-                32'h20: bpif.rdata = i2c_t_high;
+                32'h1C: bpif.rdata[CLKDIV_BITS-1:0] = i2c_t_low;
+                32'h20: bpif.rdata[CLKDIV_BITS-1:0] = i2c_t_high;
                 32'h24: begin
                     bpif.rdata = rx_rdata;
                     rx_REN = 1'b1;
